aaa7d8f6b6bba9ff42aff6e0e82b1a6db1366704
labintsev <andrej.labintsev@yandex.ru>
2024-04-25 11:56:36 +0300

    jsonify request and response

--- src/predict_app.py
+++ src/predict_app.py
@@ -7,19 +7,30 @@ app = Flask(__name__)
 CORS(app)
 
 
+def predict(in_data: dict) -> int:
+    """ Predict house price from input data parameters.
+    :param in_data: house parameters.
+    :raise Error: If something goes wrong.
+    :return: House price, RUB.
+    :rtype: int
+    """
+    area = float(in_data['area'])
+    AVG_PRICE = 200_000                 # RUB / m2
+    return int(area * AVG_PRICE)
+
+
 @app.route("/")
 def home():
-    return 'Housing price service. Use /predict endpoint'
+    return '<h1>Housing price service.</h1> Use /predict endpoint'
 
 
-@app.route("/predict")
-def predict():
+@app.route("/predict", methods=['POST'])
+def predict_web_serve():
     """Dummy service"""
-    area = request.args.get("area")
-
-    price = 5_000_000
-    return f'{price}'
+    in_data = request.get_json()
+    price = predict(in_data)
+    return {'price': price}
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
