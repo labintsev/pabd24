8fb620f7d66ef614e0beca50a8c2dbeec8e2a21f
ailabintsev <ailabintsev@fa.ru>
2024-06-10 21:28:01 +0300

    del utils import

--- grads/service_test.py
+++ grads/service_test.py
@@ -4,7 +4,7 @@ import requests
 from dotenv import dotenv_values
 import pandas as pd
 
-endpoint = 'http://127.0.0.1:5000/predict'
+endpoint = 'http://192.144.14.182:8000/predict'
 
 config = dotenv_values(".env")
 HEADERS = {"Authorization": f"Bearer {config['APP_TOKEN']}"}
@@ -22,7 +22,9 @@ def do_request(data: dict) -> tuple:
 
 
 def test_100():
-    df = pd.read_csv('grads/test_100.csv')
+    df = pd.read_csv(
+        'grads/test_100.csv').head(100)
+    df['area'] = df['total_meters']
     prices = df['price']
     df = df.drop(['price'], axis=1)
     records = df.to_dict('records')
@@ -31,6 +33,7 @@ def test_100():
         t, price = do_request(row)
         delays.append(t)
         pred_prices.append(price)
+        print(f'predict: {price}')
     avg_delay = sum(delays) / len(delays)
     error = np.array(pred_prices) - prices.to_numpy()
     avg_error = np.mean(error)
